30a927dbac78967ebe10bf353ac202f57ef9e0f8
httgiang <httgiang.gdsciu@gmail.com>
2024-06-15 00:38:18 +0700

    Minor adjustment

--- src/main/java/com/example/sudokugame/ui/MenuController.java
+++ src/main/java/com/example/sudokugame/ui/MenuController.java
@@ -4,15 +4,19 @@ import com.example.sudokugame.core.Game;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MenuController {
     private Stage stage;
+    @FXML
+    private Label instruction;
     @FXML
     private void switchToPlay(ActionEvent event) throws IOException {
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
