b6bb5d9d98bcfb6c89dd82ffd7b1aea504a794a5
Itsjennyyahh <56812916+Jenniferduong20012004@users.noreply.github.com>
2024-06-19 00:24:41 +0700

    add

--- src/main/java/com/example/sudokugame/ui/SolveController.java
+++ src/main/java/com/example/sudokugame/ui/SolveController.java
@@ -2,11 +2,13 @@ package com.example.sudokugame.ui;
 
 import com.example.sudokugame.core.Level;
 import com.example.sudokugame.util.LoadMethods;
+import com.example.sudokugame.util.Pair;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -14,11 +16,14 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import static com.example.sudokugame.util.Constants.SUDOKU_SIZE;
 
 public class SolveController implements Initializable {
+    public Button imprt;
+    private int [][] array ;
     private int select = -1;
     Level level;
     @FXML
@@ -100,9 +105,50 @@ public class SolveController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        initArray();
         initSudokuBoard();
-        //initInputBoard();
+        initInputBoard();
+        drawSudokuBoard(level);
     }
+
+    private void initArray() {
+        array = new int[9][9];
+        for (int i =0; i <9; i++){
+            for (int j =0; j <9; j++){
+                array[i][j]=0;
+            }
+        }
+        level = new Level(array);
+    }
+
+    private void initInputBoard() {
+        insertGrid.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                if (select == -1) {
+                    return;
+                } else {
+                    imprt.setDisable(true);
+                    for (Node node : insertGrid.getChildren()) {
+                            if (node instanceof TextField) {
+                                if (node.localToScene(node.getBoundsInLocal()).contains(e.getSceneX(), e.getSceneY())) {
+                                    TextField textfield = (TextField) node;
+                                    int row = select / SUDOKU_SIZE;
+                                    int col = select % SUDOKU_SIZE;
+                                    int value = Integer.parseInt(textfield.getText());
+                                    ArrayList<Pair<Integer, Integer>> row_col_pairs = new ArrayList<>();
+                                    level.insert(row, col, value, row_col_pairs);
+                                }
+                            }
+                        }
+
+                        select = -1;
+                        drawSudokuBoard(level);
+                }
+            }
+        });
+    }
+
     private void initSudokuBoard() {
         for (int row = 0; row < SUDOKU_SIZE; row++) {
             for (int col = 0; col < SUDOKU_SIZE; col++) {
